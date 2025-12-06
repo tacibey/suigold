73211ccd2d5580866fe586c66962b28d236d7d86
Tacibey <taciyalcin@gmail.com>
2025-12-06 16:44:16 +0300

    Fix: Netlify config and static export settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // BU SATIR HAYATİ ÖNEM TAŞIYOR
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
