27384e4f269140ecb9d633094f8b8612f4caf226
Tacibey <taciyalcin@gmail.com>
2025-12-06 17:59:26 +0300

    Fix: Force install dependencies and transpile wallet kit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Web3 paketlerini derleyiciye tanıtıyoruz
+  transpilePackages: ['@suiet/wallet-kit', '@mysten/dapp-kit', '@mysten/sui.js'],
 };
 
 export default nextConfig;
